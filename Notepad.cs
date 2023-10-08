1ab95c9a86e45cb77147fb45b4267edba000f8f4
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2023-10-07 23:49:05 -0700

    Add project files.

---
+++ Notepad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoDB_Project
+{
+    internal class Notepad
+    {
+        private string _makeNoteOf;
+        private string _userEntry1;
+        private string _userEntry2;
+        private List<Note> _notes = new List<Note>();
+        public Notepad() { }
+    }
+}
